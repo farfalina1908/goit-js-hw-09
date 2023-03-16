194c450d64ad8130e9585c3d25f20d2221de1e5d
Elena Berezina <farfalina1908@gmail.com>
2023-03-16 15:43:15 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,19 @@
+const bodyEl = document.querySelector('body');
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
-
-btnStart.addEventListener("click", startChangeBgColor);
-btnStop.addEventListener("click", stopChangeBgColor);
-
-function startChangeBgColor() {
-   body.style.background: getRandomHexColor();
-};
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
+
+ let getId = null;
+
+btnStart.addEventListener("click", startChangeBgColor);
+btnStop.addEventListener("click", stopChangeBgColor);
+
+function startChangeBgColor(event) {
+  getId = setInterval(() => bodyEl.style.backgroundColor = getRandomHexColor(), 1000);
+  btnStart.disabled  = true;
+  btnStop.disabled  = false;
+}
+
