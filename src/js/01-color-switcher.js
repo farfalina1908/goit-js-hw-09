f125a47b6142904c8a2f48031ac76769d0b66987
Elena Berezina <farfalina1908@gmail.com>
2023-03-16 15:25:41 +0200

    make 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener("click", startChangeBgColor);
+btnStop.addEventListener("click", stopChangeBgColor);
+
+function startChangeBgColor() {
+   body.style.background: getRandomHexColor();
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
