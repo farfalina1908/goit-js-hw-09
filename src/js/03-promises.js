0ad32f643b0c524579d22a4cfe7b0a4f935f3a7d
Elena Berezina <farfalina1908@gmail.com>
2023-03-17 16:20:21 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,33 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const formEl = document.querySelector('.form');
+formEl.addEventListener('click', onSubmitForm);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() =>  {
+      if (shouldResolve) {
+      resolve({position, delay})
+    } else {
+      reject({position, delay})
+    }
+  }, delay)
+ })
 }
 
-
-createPromise(2, 1500)
+function onSubmitForm(event) {
+  event.preventDefault();
+  let timerDelay = Number(formEl.delay.value);
+  for (let i = 1; i <= formEl.amount.value; i += 1) {
+    createPromise(i, timerDelay)
   .then(({ position, delay }) => {
     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
+  timerDelay += Number(formEl.step.value);
+  }
+}
+
